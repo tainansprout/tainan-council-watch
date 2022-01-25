4eafe372b11633e08e80b4e9b272b7589776fde6
ddio <ddio@users.noreply.github.com>
2022-01-25 14:38:59 +0000

    deploy: cca32e47783aecf7eb73f71f7e5ff1a32f7f02e8

---
+++ _nuxt/static/1643121535/councilors/蔡筱薇/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/councilors/%E8%94%A1%E7%AD%B1%E8%96%87", (function(a,b,c){return {data:[{councilor:{slug:b,name:b,area:"第十選區",sayList:[{target:a,summary:a,say:a,date:"2019\u002F5\u002F10",src:[null]}],dir:"\u002Fcouncilors",path:"\u002Fcouncilors\u002F蔡筱薇",extension:".json",createdAt:c,updatedAt:c}}],fetch:{},mutations:void 0}}("","蔡筱薇","2022-01-25T14:38:27.186Z")));
