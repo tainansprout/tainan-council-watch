4a042c2b89e48f1a431fa8caa7e6fc849f5b38ae
ddio <ddio@users.noreply.github.com>
2022-05-05 04:58:47 +0000

    deploy: 0426d3fbc80c5aa2021b022d4c2fa6dfc52b17b1

---
+++ _nuxt/static/1651726716/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftainan-council-watch\u002F_nuxt\u002Fstatic\u002F1651726716",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftainan-council-watch\u002F",assetsPath:"\u002Ftainan-council-watch\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"04551fd1"}}}}(null));
