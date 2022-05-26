fe07b11bd1eaa1c65d52ffe5a938ba4928794abb
ddio <ddioibb@gmail.com>
2022-05-26 11:47:15 +0800

    feat: import icon set and default og image

---
+++ tcw-ui/components/TcwIcon.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  img.tcwIcon(:class="[`tcwIcon--${size}`]" :src="src")
+</template>
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    size: {
+      type: String,
+      default: 'regular',
+      validator (size) {
+        return size === 'regular' || size === 'large'
+      }
+    }
+  },
+  computed: {
+    src () {
+      return `/icons/${this.icon}.svg`
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.tcwIcon {
+  font-size: 1rem;
+  width: 1em;
+  height: 1em;
+  object-fit: contain;
+
+  &--large {
+    font-size: 1.5rem;
+  }
+}
+</style>
