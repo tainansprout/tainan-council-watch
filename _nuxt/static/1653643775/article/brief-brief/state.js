d921b7a8e7065d27239bb0ad14a360cf403f882a
ddio <ddio@users.noreply.github.com>
2022-05-27 12:27:53 +0000

    deploy: 6062be20b655ddbdd21d9ec670a3aa671569daa2

--- _nuxt/static/1653643775/article/brief-brief/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1653643775",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002Fbrief-brief",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1ead74c0"}}}}(null));
