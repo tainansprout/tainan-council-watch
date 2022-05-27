68954a2cd9effce580059b95914fc164a4791afb
ddio <ddioibb@gmail.com>
2022-05-27 23:43:22 +0800

    feat: add hero section

---
+++ tcw-ui/components/LandingCover.vue
@@ -0,0 +1,82 @@
+<template lang="pug">
+  .landingCover.relative
+    img.w-100.db.dn-ns(src="~/assets/cover.svg")
+    img.w-100.dn.db-ns(src="~/assets/cover-ns.svg")
+    .landingCover__header.absolute
+      h1.landingCover__title.fw5 台南市議會觀測站
+      .landingCover__subtitle(role="doc-subtitle") {{roundLabel}}
+
+</template>
+<script>
+import { number2zh } from '~/libs/utils'
+import { NUMBER_2_ZH } from '~/libs/defs'
+
+const START_YEAR = 2010
+const YEAR_PER_ROUND = 4
+
+export default {
+  props: {
+    round: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    roundLabel () {
+      const roundZh = number2zh(this.round)
+      const roundNumber = NUMBER_2_ZH.indexOf(roundZh)
+
+      const yearStart = START_YEAR + (roundNumber - 1) * YEAR_PER_ROUND
+      const yearEnd = yearStart + YEAR_PER_ROUND
+
+      return `${yearStart}-${yearEnd} ${roundZh}屆議會`
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.landingCover {
+  line-height: normal;
+  &__title {
+    color: #282828;
+    letter-spacing: 2px;
+    font-size: 5.75vw;
+    margin: 0;
+  }
+  &__subtitle {
+    color: #7a7c7d;
+    font-size: 3.75vw;
+    letter-spacing: 1px;
+    margin-top: 0.5rem;
+  }
+  &__header {
+    top: 13vw;
+    left: 7vw;
+  }
+  @include not-small-screen {
+    &__title {
+      letter-spacing: 4px;
+    }
+    &__subtitle {
+      letter-spacing: 3px;
+    }
+  }
+  @media screen and (min-width: 70em) {
+    left: -1.5rem;
+    right: -1.5rem;
+
+    &__header {
+      top: 8rem;
+      left: 5rem;
+    }
+    &__title {
+      letter-spacing: 4px;
+      font-size: 3.5rem;
+    }
+    &__subtitle {
+      font-size: 2.25rem;
+      letter-spacing: 3px;
+    }
+  }
+}
+</style>
--- tcw-ui/pages/_round/index.vue
+++ tcw-ui/pages/_round/index.vue
@@ -1,10 +1,7 @@
 <template lang="pug">
-  .landing.mw8.ph3.center
+  .landing.pageContainer
     .landing__entry.mt5
-      .landing__header.relative.flex.items-center.justify-center
-        .absolute.top-0.left-0.w4.h4.bw2.bl.bt.b--light-yellow
-        .absolute.bottom-0.right-0.w4.h4.bw2.bb.br.b--light-yellow
-        h1.f2.f1-l.fw5 台南市議會觀測站
+      landing-cover(:round="round")
       .landing__navLinks.flex.justify-between.mw6.center.mt4.mt5-l
         nuxt-link.f4.f3-l(
           v-for="link in navLinks"
