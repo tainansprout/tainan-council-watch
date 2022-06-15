7372ae843af350b340ca223331450595334e7e59
littlesea922 <littlesea922@users.noreply.github.com>
2022-06-15 07:02:30 +0000

    deploy: aa73885f3c5a3585a3886185039d35db659e9e86

--- _nuxt/static/1655276495/3rd/district/9th/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655276495",layout:"default",error:a,serverRendered:true,routePath:"\u002F3rd\u002Fdistrict\u002F9th",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e2536c27"}}}}(null));
--- _nuxt/static/1655276495/article/brief-brief/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655276495",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002Fbrief-brief",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e2536c27"}}}}(null));
