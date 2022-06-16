c63e9076ece2b439ce4cb2614cd28b66fe2f3dde
littlesea922 <littlesea922@users.noreply.github.com>
2022-06-16 05:26:37 +0000

    deploy: f795daca033a840edfa0c1a576c139661f9447ba

--- _nuxt/static/1655356233/3rd/district/6th/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1655356233",layout:"default",error:a,serverRendered:true,routePath:"\u002F3rd\u002Fdistrict\u002F6th",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f3c20337"}}}}(null));
