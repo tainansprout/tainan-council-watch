0ca54a6dc14c4df93f9d0451ba07030d0d7da857
ddio <ddio@users.noreply.github.com>
2022-07-03 21:18:11 +0000

    deploy: 9760a2353e6c5bb9e5b279b4035b2edb56707271

---
+++ _nuxt/static/1656883056/3rd/councilor/3B7EB972/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656883056",layout:"default",error:a,serverRendered:true,routePath:"\u002F3rd\u002Fcouncilor\u002F3B7EB972",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8094d5e2"}}}}(null));
