eb95df03fc0ff1f0fd39051c4a89e2e77157bf6b
littlesea922 <littlesea922@users.noreply.github.com>
2022-07-05 06:28:12 +0000

    deploy: c31b2fb0abb31283d87986ed53be49b994dc7137

---
+++ _nuxt/static/1657002447/article/budget-111/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1657002447",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002Fbudget-111",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bfcb5a9d"}}}}(null));
