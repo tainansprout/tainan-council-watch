640834bd7fed8c00f4325ba00b361018cbe3937f
ddio <ddioibb@gmail.com>
2022-07-11 11:36:29 +0800

    feat: add about page

--- tcw-ui/layouts/default.vue
+++ tcw-ui/layouts/default.vue
@@ -10,3 +10,41 @@ export default {
   name: 'TcwDefault'
 }
 </script>
+<style lang="scss" scoped>
+main {
+  ::v-deep article .nuxt-content {
+    line-height: normal;
+    h1, h2, h3 {
+      margin: 2rem 0 1rem;
+      a:first-child {
+        display: none;
+      }
+    }
+    h1, h2 {
+      font-size: 1.25rem;
+    }
+
+    h3 {
+      font-size: 1rem;
+    }
+
+    p + p {
+      margin-top: 1rem;
+    }
+
+    h1 {
+      margin-bottom: 0.75rem;
+    }
+
+    @include not-small-screen {
+      h1 {
+        margin-bottom: 1.5rem;
+      }
+
+      .nuxt-content {
+        line-height: 1.75;
+      }
+    }
+  }
+}
+</style>
---
+++ tcw-ui/pages/about.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  article.about.pageContainer
+    h1.f3.ls4.mt5 {{article.title}}
+    nuxt-content.ls2.lh(:document="article")
+</template>
+<script>
+import { friendlyHeader } from '~/libs/crawlerFriendly'
+export default {
+  async asyncData ({ $content, params, redirect }) {
+    const article = await $content('meta/about').fetch()
+    return { article }
+  },
+  head: friendlyHeader({
+    title () { return this.article.title }
+  })
+}
+</script>
+<style lang="scss" scoped>
+.about {
+}
+</style>
