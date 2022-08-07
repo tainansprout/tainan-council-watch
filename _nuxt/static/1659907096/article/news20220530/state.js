04422437719464b6d02ca80b855c5183698e7b1a
ddio <ddio@users.noreply.github.com>
2022-08-07 21:18:55 +0000

    deploy: 0eede7a6bf3ca5588d35a6ed4cf6aa939eed595c

---
+++ _nuxt/static/1659907096/article/news20220530/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659907096",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002Fnews20220530",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"99d6ac77"}}}}(null));
