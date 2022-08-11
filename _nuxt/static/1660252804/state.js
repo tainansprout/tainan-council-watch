47c31a058261be0c7516a3b8d07918789a7ba977
ddio <ddio@users.noreply.github.com>
2022-08-11 21:20:46 +0000

    deploy: 0eede7a6bf3ca5588d35a6ed4cf6aa939eed595c

---
+++ _nuxt/static/1660252804/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1660252804",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5057186b"}}}}(null,"\u002F"));
