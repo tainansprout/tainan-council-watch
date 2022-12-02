a27efa099cd877bfcce22c755829a9e7bc5f7d56
ddio <ddio@users.noreply.github.com>
2022-12-02 21:17:36 +0000

    deploy: c04007766104202ce619698a8cfc9e3f32ddeb26

---
+++ _nuxt/static/1670015809/article/budget-111/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1670015809",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002Fbudget-111",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cb9a771b"}}}}(null));
