76f9aa66da7f22f4575eb300c6801974a2f5ccde
ddio <ddio@users.noreply.github.com>
2023-01-14 21:15:39 +0000

    deploy: 0872b8a6325959e2c7a5e8b2a3810806ba8f69ef

--- _nuxt/static/1673644600/3rd/interpellation/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673644600",layout:"default",error:a,serverRendered:true,routePath:"\u002F3rd\u002Finterpellation",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e14909c2"}}}}(null));
