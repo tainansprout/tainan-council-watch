88601bf532b6fd4c830ef85431ca6a4cb3cd42d2
littlesea922 <littlesea922@users.noreply.github.com>
2023-02-22 08:36:20 +0000

    deploy: dad1cae8ad2ff166e5f50e0e963d1d730ccecc82

--- _nuxt/static/1677054322/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1677054322",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ef3e7de6"}}}}(null,"\u002F"));
