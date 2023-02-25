8ac1f441844863d713968f2496f8a993374dfaaa
ddio <ddio@users.noreply.github.com>
2023-02-25 21:16:58 +0000

    deploy: dad1cae8ad2ff166e5f50e0e963d1d730ccecc82

--- _nuxt/static/1677273378/3rd/district/8th/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1677273378",layout:"default",error:a,serverRendered:true,routePath:"\u002F3rd\u002Fdistrict\u002F8th",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"de239716"}}}}(null));
