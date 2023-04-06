700057f11bf5ad96658580781fcdac7579e5eaad
littlesea922 <littlesea922@users.noreply.github.com>
2023-04-06 08:18:08 +0000

    deploy: 6e96f1ec5d0207a80266840acc940142f46486e9

--- _nuxt/static/1680766530/article/category/agenda/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1680766530",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticle\u002Fcategory\u002Fagenda",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0fb166f3"}}}}(null));
