3c35329cd2b3740dbd229dfa9f20ea0dd98e8857
littlesea922 <littlesea922@users.noreply.github.com>
2023-05-15 07:23:37 +0000

    deploy: c1074e8b0b990245bfa292d8d368b48c1c254415

--- _nuxt/static/1684135218/4th/councilor/B219417D/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1684135218",layout:"default",error:a,serverRendered:true,routePath:"\u002F4th\u002Fcouncilor\u002FB219417D",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"619b9a2e"}}}}(null));
---
+++ _nuxt/static/1684135320/4th/councilor/E8CDE770/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1684135320",layout:"default",error:a,serverRendered:true,routePath:"\u002F4th\u002Fcouncilor\u002FE8CDE770",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d333cf6b"}}}}(null));
